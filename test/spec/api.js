2299ac0750e9518e6131235ee41640c5313c351f
Il Jae Lee <iljae@umich.edu>
2016-03-12 21:03:22 -0500

    Fixed JSlint style errors.

--- test/spec/api.js
+++ test/spec/api.js
@@ -1,4 +1,4 @@
-/* global MichiganResource */
+/* global MFat, chai */
 
 (function () {
   'use strict';
@@ -8,14 +8,13 @@
 
     it('should load dining hall info from the Michigan\'s server.',
        function (done) {
-         var resource = new MichiganResource('Bursley Dining Hall');
-         var request = resource.retrieve();
+         var request = MFat.scrap('Bursley Dining Hall');
 
-         request.done(function() {
-           assert(resource.menus.length != 0);
-           for(var i in resource.menus) {
-             var meals = resource.menus[i];
-             assert.isAbove(meals.length, 0);
+         request.done(function(menus) {
+           chai.assert(menus.length !== 0);
+           for(var i in menus) {
+             var meals = menus[i];
+             chai.assert.isAbove(meals.length, 0);
            }
 
            done();
--- test/spec/reduce.js
+++ test/spec/reduce.js
@@ -1,45 +1,39 @@
+/* global MFat, chai, _ */
+
 (function () {
     'use strict';
 
     describe('Find the most optimal values to eat.', function () {
-        var menu = [
-          {min: 5, max: 1},
-          {min: 15, max: 1},
-          {min: 50, max: 10},
-          {min: 40, max: 9},
-          {min: 200, max: 50},
-        ];
-
         it('return the correct number', function () {
-          MFat.collection(menu, "min", "max");
+          var menu = [
+            {min: 5, max: 1},
+            {min: 15, max: 1},
+            {min: 50, max: 10},
+            {min: 40, max: 9},
+            {min: 200, max: 50}
+          ];
 
-          {
-            var result = MFat.optimize(150);
-            var totalMin = _.reduce(result.collection, function(memo, val) {return memo + val.min;}, 0);
-            chai.assert.isTrue(totalMin <= 150);
-            chai.assert.equal(result.accumulator, 33);
-          }
+          MFat.collection(menu, 'min', 'max');
 
-          {
-            var result = MFat.optimize(160);
-            var totalMin = _.reduce(result.collection, function(memo, val) {return memo + val.min;}, 0);
-            chai.assert.isTrue(totalMin <= 160);
-            chai.assert.equal(result.accumulator, 36);
-          }
+          var result = MFat.optimize(150);
+          var totalMin = _.reduce(result.collection, function(memo, val) { return memo + val.min; }, 0);
+          chai.assert.isTrue(totalMin <= 150);
+          chai.assert.equal(result.accumulator, 33);
 
-          {
-            var result = MFat.optimize(170);
-            var totalMin = _.reduce(result.collection, function(memo, val) {return memo + val.min;}, 0);
-            chai.assert.isTrue(totalMin <= 170);
-            chai.assert.equal(result.accumulator, 38);
-          }
+          result = MFat.optimize(160);
+          totalMin = _.reduce(result.collection, function(memo, val) { return memo + val.min; }, 0);
+          chai.assert.isTrue(totalMin <= 160);
+          chai.assert.equal(result.accumulator, 36);
 
-          {
-            var result = MFat.optimize(200);
-            var totalMin = _.reduce(result.collection, function(memo, val) {return memo + val.min;}, 0);
-            chai.assert.isTrue(totalMin <= 200);
-            chai.assert.equal(result.accumulator, 50);
-          }
+          result = MFat.optimize(170);
+          totalMin = _.reduce(result.collection, function(memo, val) { return memo + val.min; }, 0);
+          chai.assert.isTrue(totalMin <= 170);
+          chai.assert.equal(result.accumulator, 38);
+
+          result = MFat.optimize(200);
+          totalMin = _.reduce(result.collection, function(memo, val) { return memo + val.min; }, 0);
+          chai.assert.isTrue(totalMin <= 200);
+          chai.assert.equal(result.accumulator, 50);
         });
     });
 })();
